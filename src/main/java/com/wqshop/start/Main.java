1eedcf8bf09101714d667f43bece1ea326ea7b20
gzq <694326243@qq.com>
2016-10-26 10:44:24 +0800

    提交wqshop基础框架

---
+++ src/main/java/com/wqshop/start/Main.java
@@ -0,0 +1,12 @@
+package com.wqshop.start;
+
+/**
+ * Created by admin on 2016/10/26.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("start");
+    }
+}
